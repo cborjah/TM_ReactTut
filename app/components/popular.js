9f9f80b65b3782d99bc8d782422264f492bc73cd
Christian Borja <cpborja32@gmail.com>
2017-05-09 15:30:51 -0700

    finishes section 5

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 
+// If component only has a render method, is stateless, takes in props, and just renders UI,
+// use a stateless functional component
+const SelectLanguage = (props) => {
+  let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className='languages'>
+      {languages.map((lang) => {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+            onClick={() => props.onSelect(lang)}
+            key={lang}>
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
 class Popular extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+  }
+
+  updateLanguage(lang) {
+    this.setState({ selectedLanguage: lang });
+  }
+
   render() {
     return (
-      <div>Popular!</div>
+      <SelectLanguage
+        selectedLanguage={this.state.selectedLanguage}
+        onSelect={(lang) => this.updateLanguage(lang)}
+      />
     );
   }
 }
