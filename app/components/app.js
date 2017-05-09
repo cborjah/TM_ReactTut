3595d00df4f2439d3947f56e1a7c968e8d4c182c
Christian Borja <cpborja32@gmail.com>
2017-05-09 16:48:32 -0700

    finishes section 7

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import Popular from './popular';
+import ReactRouter, { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './nav';
+import Home from './home';
+import Battle from './battle';
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <NavBar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            {/* With Switch, this last route will always fire if none of the previous routes are hit. */}
+            <Route render={() => <p>Page Not Found</p>}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,23 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    // Add publicPath so when the page is refreshed, ReactRouter can take over
+    // and load the appropriate component. Else, the app will ping the server for
+    // the assets.
+    publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
     ]
+  },
+  // Add devServer so when the page is refreshed, ReactRouter can take over
+  // and load the appropriate component Else, the app will ping the server for
+  // the assets.
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [new HtmlWebpackPlugin({
     template: 'app/index.html'
