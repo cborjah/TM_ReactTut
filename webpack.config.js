51c09a0c99c75ebc611713aa8a8d107abb9ac029
Christian Borja <cpborja32@gmail.com>
2017-05-08 18:19:25 -0700

    sets up webpack-dev-server, finishes section 1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/index/js',
+  entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
     ]
   },
   plugins: [new HtmlWebpackPlugin({
