2f6269cd7d65ee75dd871dec52fc82693b6e0d21
Christian Borja <cpborja32@gmail.com>
2017-05-16 18:27:50 -0700

    finishes section 10

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,56 @@
 import axios from 'axios';
 
+function getProfile(username) {
+  return axios.get('https://api.github.com/users/' + username)
+    .then((user) => user.data);
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + '&per_page=100')
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce((count, repo) => count + repo.stargazers_count))
+}
+
+function calculateScore(profile, repos) {
+  let followers = profile.followers;
+  let totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then((data) => {
+    let profile = data[0];
+    let repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export function battle(players) {
+  // players.map(getUserData) is shorthand for, players.map((player) => getUserData(player))
+  return axios.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError);
+}
+
 export function fetchPopularRepos(language) {
   let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
